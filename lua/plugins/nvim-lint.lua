84fce21712384a0e0e0fb8eef920c19c4b62b04b
Esteban Forero <estebanmff@outlook.com>
2024-12-15 12:05:51 -0500

    Cleaner nvim-lint, and added some telescope features

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,55 +1,33 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-
-      -- Configure linters by filetype
-      lint.linters_by_ft = {
-        rust = {"clippy"},
-        go = {"golangcilint"},
-      }
-
-      -- Function to return a string of currently running linters
-      local function lint_progress()
-        local linters = lint.get_running()
-        if #linters == 0 then
-          -- No linters currently running
-          return ""
-        end
-        -- e.g., "󱉶 clippy" if clippy is running
-        return "󱉶 " .. table.concat(linters, ", ")
-      end
-
-      -- Example: Integrate with your statusline
-      -- If you're using a simple statusline, you might do something like:
-      -- vim.o.statusline = "%f %m%r%h%w [%"..lint_progress().."]%y%=%l,%c%V %P"
-      --
-      -- For a more dynamic solution, integrate lint_progress() with your statusline plugin
-      -- like lualine or feline.
-
-      -- Run linting on save
-      vim.api.nvim_create_autocmd({"BufWinEnter", "BufWritePost"}, {
-        callback = function()
-          require('lint').try_lint()
-        end,
-      })
-
-
-      -- If you want to trigger linting with a keymap:
-      -- vim.keymap.set('n', '<leader>l', function() lint.try_lint() end, { desc = "Run linters" })
-
-      -- Optional: Configure diagnostic display
-      -- vim.diagnostic.config({
-      --   virtual_text = true,
-      --   signs = true,
-      --   update_in_insert = false,
-      -- })
-
-      -- Make lint_progress function accessible globally or in your statusline config
-      _G.lint_progress = lint_progress
-    end,
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
   },
-}
+  config = function()
+    local lint = require("lint")
 
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      python = { "pylint" },
+      rust = {"clippy"},
+      go = {"golangcilint"},
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = lint_augroup,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>l", function()
+      lint.try_lint()
+    end, { desc = "Trigger linting for current file" })
+  end,
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,13 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    -- Ensure nvim-lint is installed if not already
+    'mfussenegger/nvim-lint',
   },
   config = function()
     local telescope = require('telescope')
     local builtin = require('telescope.builtin')
+    local lint = require("lint")
 
     telescope.setup({
       defaults = {
@@ -76,11 +79,31 @@ return {
       })
     end, { desc = '[P]roject [W]arnings (LSP) in all open buffers' })
 
-    -- Additional LSP related pickers
+    -- LSP related pickers
     vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = 'LSP References' })
     vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'LSP Definitions' })
     vim.keymap.set('n', 'gi', builtin.lsp_implementations, { desc = 'LSP Implementations' })
     vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, { desc = 'LSP Type Definitions' })
+
+    -- Custom picker to show currently running linters
+    vim.keymap.set('n', '<leader>pl', function()
+      local running_linters = lint.get_running()
+
+      if #running_linters == 0 then
+        -- If no linters are running, show a simple message
+        print("No linters currently running.")
+        return
+      end
+
+      require('telescope.pickers').new({}, {
+        prompt_title = "Currently Running Linters",
+        finder = require('telescope.finders').new_table {
+          results = running_linters,
+        },
+        sorter = require('telescope.config').values.generic_sorter({}),
+      }):find()
+    end, { desc = '[P]roject [L]inters currently running' })
+
   end
 }
 
