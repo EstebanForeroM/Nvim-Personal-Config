23a445a6a7ba14f0f41f9e15603154260424474f
Esteban Forero <estebanmff@outlook.com>
2024-12-16 18:28:00 -0500

    Added complete lua snippet

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,18 @@ return {
     },
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
+
+      local ls = require("luasnip")
+
+      vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<C-E>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
     end
   },
   {
@@ -20,6 +32,8 @@ return {
     config = function()
       local cmp = require('cmp')
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+      local luasnip = require("luasnip")
 
       cmp.setup({
         sources = {
@@ -32,6 +46,27 @@ return {
           ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
           ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           ['<C-Space>'] = cmp.mapping.complete(),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
         }),
         snippet = {
           expand = function(args)
